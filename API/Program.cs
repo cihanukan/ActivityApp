58e815e6c203bd261aa05ccec67bc1c35d5c9a61
cihan_ukan <cihan.ukan@gmail.com>
2021-03-31 23:12:14 +0300

    Added EF DB Context & Code First Migration & Created Database

--- API/Program.cs
+++ API/Program.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Persistence;
 
 namespace API
 {
@@ -13,7 +16,28 @@ namespace API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using var scope = host.Services.CreateScope();
+
+            var services = scope.ServiceProvider;
+
+            //Checking if the database exist or any migration left behind
+            try
+            {
+                var context = services.GetRequiredService<DataContext>();
+                context.Database.Migrate();
+            }
+            //Throw error if any error occured during migration
+            catch (System.Exception ex)
+            {
+                
+                var logger = services.GetRequiredService<ILogger<Program>>();
+                logger.LogError(ex, " An error occured during migration");
+            }
+            
+            //if everything is ok then run the application
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
